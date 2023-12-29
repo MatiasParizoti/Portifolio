497dfb0ff68127f33d377b6b1e63a69d7ce60bbb
Matheus Parizoti <134006837+MatiasParizoti@users.noreply.github.com>
2023-12-29 16:36:52 -0300

    Add files via upload

---
+++ revel.js
@@ -0,0 +1,96 @@
+window.revelar = ScrollReveal({reseat:true})
+
+//TOPO DO SITE
+revelar.reveal('.efeito-txt-topo',{
+    duration:2000,
+    distance: '90px'
+})
+revelar.reveal('.efeito-img-topo',{
+    duration:2000,
+    distance: '90px',
+    delay: 500
+})
+
+// OS h2
+revelar.reveal('.efeito-h2',{
+    duration:2000,
+    distance: '90px'
+})
+// SESSÃO ESPECIALIDADES
+revelar.reveal('.efeito-especialidades1',{
+    duration:2000,
+    distance: '90px',
+    delay:500
+})
+revelar.reveal('.efeito-especialidades2',{
+    duration:2000,
+    distance: '90px',
+    delay:1000
+})
+revelar.reveal('.efeito-especialidades3',{
+    duration:2000,
+    distance: '90px',
+    delay:1500
+})
+
+//SESSÃO SOBRE 
+
+revelar.reveal('.efeito-meu',{
+    duration:2000,
+    distance: '90px',
+    delay:500,
+    origin: 'top'
+})
+revelar.reveal('.efeito-meu-txt',{
+    duration:2000,
+    distance: '90px',
+    delay:1000,
+    origin: 'right'
+})
+// SESSAO ESCADA
+revelar.reveal('.efeito-escada1',{
+    duration:1000,
+    distance: '90px',
+    delay:500
+})
+revelar.reveal('.efeito-escada2',{
+    duration:1000,
+    distance: '90px',
+    delay:1000
+})
+revelar.reveal('.efeito-escada3',{
+    duration:1000,
+    distance: '90px',
+    delay:1500
+})
+// SESSÃO FORMULARIO
+revelar.reveal('.efeito-bonito1',{
+    duration:2000,
+    distance: '90px',
+    delay:500,
+    origin: 'bottom'
+})
+revelar.reveal('.efeito-bonito2',{
+    duration:2000,
+    distance: '90px',
+    delay:1000,
+    origin: 'bottom'
+})
+revelar.reveal('.efeito-bonito3',{
+    duration:2000,
+    distance: '90px',
+    delay:1500,
+    origin: 'bottom'
+})
+revelar.reveal('.efeito-bonito4',{
+    duration:2000,
+    distance: '90px',
+    delay:2000,
+    origin: 'bottom'
+})
+revelar.reveal('.efeito-bonito5',{
+    duration:2000,
+    distance: '90px',
+    delay:3000,
+    origin: 'bottom'
+})
